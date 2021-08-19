737d8c690ac937bcec22d9fe3489defa6922ee34
vietquytina <vietquytina@gmail.com>
2021-08-19 15:50:12 +0700

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int a = 5;
+            int b = 6;
             Console.WriteLine("Hello World!");
         }
     }
